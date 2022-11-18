622a5dfbfb7b7d29211345baaee42c980df125d5
rktdnjs <rktdnjs@naver.com>
2022-11-18 12:47:26 +0900

    Save

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import WriteCocktail from './screens/WriteCocktail';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import WriteCocktail from "./screens/WriteCocktail";
+import Home from "./screens/Home";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <WriteCocktail></WriteCocktail>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="칵테일 글쓰기" component={WriteCocktail} />
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/Home.js
@@ -0,0 +1,15 @@
+import { StyleSheet, View, Text } from "react-native";
+
+function Home () {
+    return (
+        <View>
+            <Text>홈 화면!!</Text>
+        </View>
+    )
+}
+
+export default Home;
+
+const styles = StyleSheet.create({
+
+})
